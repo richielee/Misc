44ceb92c5a1dfe8389d0056d69f56f652f21da9d
Richie Li <c24h42@me.com>
2023-03-15 00:34:36 +0000

    Formatting

--- pi.py
+++ pi.py
@@ -1,19 +1,19 @@
-'''Approximate pi
+"""Approximate pi
 
 Eurler famously came up with the formula:
-pi ^ 2 / 6 := sigma (1/k^2) for k = 1 to inf'''
+pi ^ 2 / 6 := sigma (1/k^2) for k = 1 to inf"""
 
 import math
 
 def approx(iterations):
-    '''approximating'''
+    """approximating"""
     tmp = 0.0
     for i in range(1, iterations + 1):
         tmp += 1.0 / (i ** 2.0)
     return math.sqrt(6 * tmp)
 
 def main():
-    '''Main function'''
+    """Main function"""
     iterations = int(input('Iteration? '))
     pi_approximated = approx(iterations)
     print("Pi is approximated as")
